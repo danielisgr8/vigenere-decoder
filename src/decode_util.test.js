9466d6e16620fa3bbbb0da36a2fb4b396c564ed4
danielisgr8 <schubertmdaniel@gmail.com>
2019-11-05 00:36:04 -0600

    Test set-up

--- src/decode_util.test.js
+++ src/decode_util.test.js
@@ -1,3 +1,25 @@
+import { getDistances } from "./decode_util";
+import { shuffle } from "./util";
+
+const randomUniqueString = (size) => {
+  if(size <= 0 || size > 26) return "";
+  const charCodes = new Array(26);
+  for(let i = 0; i < 26; i++) {
+    charCodes[i] = i + 97;
+  }
+  shuffle(charCodes);
+  //console.log(charCodes);
+  return String.fromCharCode(...(charCodes.slice(0, size)));
+}
+
 describe("getDistances", () => {
-  
+  test("base", () => {
+    for(let baseLength = 1; baseLength <= 26; baseLength++) {
+      for(let repeatCount = 1; repeatCount <= 10; repeatCount++) {
+        const str = randomUniqueString(baseLength).repeat(repeatCount);
+        if(repeatCount === 1) expect(Object.keys(getDistances(str)).length).toBe(0);
+        else expect(getDistances(str)[baseLength]).toBe(repeatCount - 1);
+      }
+    }
+  });
 });
